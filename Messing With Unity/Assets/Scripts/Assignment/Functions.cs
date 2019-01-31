7f6edba27f0e74c9ab4cbcdbeca3d5c65fd57c8b
Colt Hedin <shatsky95@yahoo.com>
2019-01-30 17:08:30 -0700

    added function and conditional examples

---
+++ Messing With Unity/Assets/Scripts/Assignment/Functions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Functions : MonoBehaviour
+void start 
+
+weather("cloudy");
+{void weather(string weatherstate){
+
+    print("the current weather is"+weatherstate);    
+ 
+ 
+    }
+}
---
+++ Messing With Unity/Assets/Scripts/Assignment/Variables.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Variables : MonoBehaviour
+// variables 
+{public int number;
+public float speed; 
+public string name;
+/*this
+is
+a
+ multiline
+ comment
+  */
+    // Start is called before the first frame update
+    void Start()
+    { number = 10;
+    speed  = 0.94f;
+    name = "bob"; 
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      transform.position = new Vector3(number,0,0);
+        
+    }
+}
