6218df9ba3334258cbf51ef86b7f6ebf05227bdb
Colt Hedin <shatsky95@yahoo.com>
2019-02-13 22:00:58 -0700

    added two more animal scripts

---
+++ Messing With Unity/Assets/Scripts/Assignment/Cat.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour {
+
+    public bool weight;
+
+    public bool whiskers;
+
+    public bool tail;
+    public bool pointyears;
+    public bool color;
+    public bool paws;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Messing With Unity/Assets/Scripts/Assignment/Dog.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog : MonoBehaviour {
+
+    public bool tail;
+    public bool paws;
+    public bool ears;
+    public bool color;
+    public bool weight;
+
+    public bool size;
+    public bool age;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
