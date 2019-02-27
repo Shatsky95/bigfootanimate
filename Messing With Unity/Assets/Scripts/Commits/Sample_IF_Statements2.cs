8dde977c49c299971d399915ce4f62118a255e8f
Colt Hedin <shatsky95@yahoo.com>
2019-02-27 15:49:32 -0700

    added my soccer player if statements and nested some as well

---
+++ Messing With Unity/Assets/Scripts/Commits/Sample_IF_Statements2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample_IF_Statements2 : MonoBehaviour {
+
+    public string isPlaying;
+    public bool socccerPlayer = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(isPlaying == "hurt"){
+           if(socccerPlayer){
+               print("I think I broke my leg");
+           }
+               
+
+               else{
+                   print("it hurts so bad");
+               }
+                   
+               
+       }            
+        else if(isPlaying == "Motivated"){
+            if(socccerPlayer){
+                print("im gonna score that goal");
+            }
+        }
+
+
+               
+           
+       
+
+        
+
+    }
+}
