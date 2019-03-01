78a0759ae8abd8ed3ea4e30ffac748b216e17e44
Colt Hedin <shatsky95@yahoo.com>
2019-02-28 21:55:55 -0700

    added another switch statement

---
+++ Messing With Unity/Assets/Scripts/Commits/Sample_Switch2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample_Switch2 : MonoBehaviour{
+
+    public string person;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        switch(person){
+            case "Monty":
+                print("Im watching shark tank");
+            break;
+            case "Tyana":
+                print("Im in my room on facebook like usual");
+            break;
+            case "Sherri":
+                print("Im making you a pizza like you wanted");
+            break;
+            default:
+                print("Ive never met "+person+"!");
+            break;
+
+        }
+        
+    }
+}
