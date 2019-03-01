1add6dd6bafd095ca873f6dc3e37062b3075b7a1
Colt Hedin <shatsky95@yahoo.com>
2019-02-28 19:58:28 -0700

    added a switch statement

--- Messing With Unity/Assets/Scripts/Assignment/Switch.cs
+++ Messing With Unity/Assets/Scripts/Assignment/Switch.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Switch : MonoBehaviour {
 
-    public string suspect;
-    public string weapon;
-    public string room;
+public string suspect;
+public string weapon;
+public string room;
 
     
 
@@ -17,26 +17,25 @@ public class Switch : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start(){
-        MurderMystery(suspect, weapon );
-    }
+         MurderMystery(suspect);
+     }
 
-    void MurderMystery(string person){
+     void MurderMystery(string person){
         switch(person){
-            case "Mr. Ketchup":
-                print("I was in the billiard room smoking a cigar and playing pool with Mr. Radish");
-            break;
+             case "Mr. Ketchup":
+                 print("I was in the billiard room smoking a cigar and playing pool with Mr. Radish");
+             break;
             case "Mr. Radish":
-                print("I was in the billiard room smoking a cigar and playing pool with Mr. Ketchup");
-            break;
-            case "Ms Frysauce":
-                print("I was talking on the rotary phone with my mother Mrs. Mayo");
-            break;
-            case "Mrs. Mayo":
-                print("I was in the kitchen cleaning up the dishes");
-            break;
-            default:
-                print("I am not familiar with "+suspect+"!");
-            break;
-        }
-    }
-}
+                 print("I was in the billiard room smoking a cigar and playing pool with Mr. Ketchup");
+             break;
+             case "Ms Frysauce":
+                 print("I was talking on the rotary phone with my mother Mrs. Mayo");
+             break;
+             case "Mrs. Mayo":
+                 print("I was in the kitchen cleaning up the dishes");
+             break;
+             default:
+                 print("I am not familiar with "+suspect+"!");
+             break;         }
+     }
+ }
---
+++ Messing With Unity/Assets/Scripts/Commits/Sample_Switch1.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample_Switch1 : MonoBehaviour {
+
+    public string character;
+
+
+
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){  
+        switch(character){
+            case "John":
+                print("I was eating lunch");
+            break;
+            case "Brad":
+                print("I was drinking coffee at starbucks");
+            break;
+            case "Zack":
+                print("I was just being lazy as usual");
+            break;
+            default:
+                print("Ive never met "+character+"!");
+            break;
+        }    
+    }
+}
+
