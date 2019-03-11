904e69e29ed61c86d3f2c5d87c62907e3a65ba2c
Colt Hedin <shatsky95@yahoo.com>
2019-03-10 23:03:31 -0600

    added more array lists

---
+++ Messing With Unity/Assets/Scripts/Commits/AL_L_andAL5.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AL_L_andAL5 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
