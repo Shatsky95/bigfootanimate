122e7ddef7ce90b2b2f1301e81c20bc0e068de9f
Colt Hedin <shatsky95@yahoo.com>
2019-03-13 17:14:33 -0600

    created a menu added my player art to character

---
+++ Messing With Unity/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour{
+//Indexed Level to Load
+    public int LevelToLoad;
+    //this function loads the level stored in the levelToLoad variable
+
+    public void LoadLevel(){
+        SceneManager.LoadScene(LevelToLoad);
+    
+    }
+//this function quits the game after the button is pressed
+    public void LevelExit(){
+        Application.Quit();
+    }
+
+
+
+  
+}
