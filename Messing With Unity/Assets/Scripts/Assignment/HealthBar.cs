251a71fc349983111d0efffef1bbed0396b8acc6
Colt Hedin <shatsky95@yahoo.com>
2019-03-20 18:01:12 -0600

    tinkered with a healthbar system

---
+++ Messing With Unity/Assets/Scripts/Assignment/HealthBar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour{
+
+    public int playerHealth = 100;
+    public int currentHealth;
+
+    void Start (){
+        currentHealth = playerHealth;
+    }
+    
+
+    
+    
+
+    // Update is called once per frame
+    void Update(){
+
+    }
+
+    public void TakeDamage(int amount){
+        currentHealth -= amount;
+        if(currentHealth <= 0){
+                currentHealth=0;
+                print("GAME OVER");
+        }
+    }
+        
+    }
+
