53c7ecb0afa24a7dce2a35adbfca51dffb0e5436
Colt Hedin <shatsky95@yahoo.com>
2019-03-25 15:40:22 -0600

    added a fish script

---
+++ Messing With Unity/Assets/Scripts/Commits/Fish.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour {
+
+    public bool size;
+    public bool weight;
+
+    public bool color;
+
+    public bool species;
+    public bool toughness;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
