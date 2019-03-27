c2b4da1a341adcd95837724058803115c76153b8
Colt Hedin <shatsky95@yahoo.com>
2019-03-27 17:07:31 -0600

    learned more loops

---
+++ Messing With Unity/Assets/Scripts/Assignment/LevelManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour{
+    public GameObject CurrentCheckPoint;
+    private Rigidbody2D pcRigid;
+    private GameObject player;
+
+    //particles 
+    public GameObject deathParticle;
+    public GameObject respawnParticle;
+
+    //respawn delay
+    public float respawnDelay;
+
+    //point penalty on death 
+    public int pointPenaltyOnDeath;
+
+    //store gravity value
+    private float gravityStore;
+
+
+    // Start is called before the first frame update
+    void Start(){
+        pcRigid = GameObject.Find("player").GetComponent<Rigidbody2D>();
+        player = GameObject.Find("player");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Messing With Unity/Assets/Scripts/Assignment/Loops.cs
+++ Messing With Unity/Assets/Scripts/Assignment/Loops.cs
@@ -3,17 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Loops : MonoBehaviour {
-     public int bottles = 100;
-
+    public int bottles = 100;
+    public bool isRaining = true;
 
     // Start is called before the first frame update
     void Start() {
+        //do while loop
+        // bool shouldContinue = false;
+
+        // do{
+        //     print("Hello World");
+        // }while(shouldContinue == true);
+
         
         //For loop
-        for(int i=0; i < bottles; i++){
-            print(i +"bottles of beer on the wall");
-        }
+        //  for(int i=0; i < bottles; i++){
+        //      print(i +"bottles of beer on the wall");
+        //  }
         
+        //  while(bottles <= 100){
+        //     print(bottles +"bottles of ginger beer on the wall");
+        // }
+        //For Each Loop
+
+        string[] strings = new string[3];
+
+        strings[0] = "first string";
+        strings[1] = "second string";
+        strings[2] = "third string";
+
+        foreach(string item in strings){
+            print (item);
+        }
+
+
     }
 
     // Update is called once per frame
