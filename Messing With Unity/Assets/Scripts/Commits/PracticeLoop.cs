b79b5b94b60d8111878aafdb7d26ec7da71edc95
Colt Hedin <shatsky95@yahoo.com>
2019-03-28 22:29:53 -0600

    practiced some loop scripts

---
+++ Messing With Unity/Assets/Scripts/Commits/PracticeLoop.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeLoop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start(){
+        bool goodluck = false;
+        do{
+            print("you are gonna need it");
+        }while(goodluck == true);
+
+        string[] birds = new string[3];
+        birds[0] = "first birds";
+        birds[1] = "second birds";
+        birds[2] = "third birds";
+
+        foreach(string animal in birds){
+            print(animal);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
