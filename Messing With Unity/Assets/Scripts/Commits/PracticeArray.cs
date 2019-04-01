c363c831e3e50a3ee280f4acc566a54a666846f3
Colt Hedin <shatsky95@yahoo.com>
2019-03-31 20:50:39 -0600

    wrote a loop, array, switch scripts

---
+++ Messing With Unity/Assets/Scripts/Commits/PracticeArray.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeArray : MonoBehaviour{
+
+    public string spider1 = "bob";
+    public string spider2 = "jane";
+    public string spider3 = "clark";
+    public string spider4 = "thor";
+
+    public string[] spiderlist = new string[]{"bob","jane","clark","thor"};
+
+    // Start is called before the first frame update
+    void Start(){
+    
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Messing With Unity/Assets/Scripts/Commits/SwitchPractice1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchPractice1 : MonoBehaviour{
+
+    public string car;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        switch(car){
+            case "chevy":
+            case "ford":
+            case "buick":
+            print("We all drive fast");
+            break;
+        }
+        
+    }
+}
