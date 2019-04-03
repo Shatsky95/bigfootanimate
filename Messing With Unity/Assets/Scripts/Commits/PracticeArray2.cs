0389fed1034d7202c8cc34409f9d5f70dbbb2e64
Colt Hedin <shatsky95@yahoo.com>
2019-04-02 18:16:53 -0600

    pratices some array scripts

---
+++ Messing With Unity/Assets/Scripts/Commits/PracticeArray2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour{
+    public string car1 = "Ford";
+    public string car2 = "Chevy";
+    public string car3 = "Dodge";
+    public string car4 = "Kia";
+
+    public string[] carlist = new string[]{"Ford","Chevy","Dodge","Kia"};
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Messing With Unity/Assets/Scripts/Commits/PracticeArray3.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeArray3 : MonoBehaviour{
+    public string book1 = "The Shining";
+    public string book2 = "IT";
+    public string book3 = "Pet Sematary";
+    public string book4 = "The Green Mile";
+
+    public string[] booklist = new string[]{"The Shining","IT","Pet Sematary","The Green Mile"};
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
