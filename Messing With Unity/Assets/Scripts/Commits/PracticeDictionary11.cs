2f352d471faf3ec706eb52bb50891cb442d2197a
Colt Hedin <shatsky95@yahoo.com>
2019-04-11 21:37:03 -0600

    made more additions to level 3

---
+++ Messing With Unity/Assets/Scripts/Commits/PracticeDictionary11.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeDictionary11 : MonoBehaviour
+{ public Hashtable tyanainfo = new Hashtable();
+    // Start is called before the first frame update
+    void Start()
+    {   tyanainfo.Add("firstName", "Tyana");
+        tyanainfo.Add("lastName", "Hedin");
+        tyanainfo.Add("Gender", "Female");
+        tyanainfo.Add("isMarried", false);
+        tyanainfo.Add("age", 16);
+
+        Debug.Log((string)tyanainfo["lastName"]);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
