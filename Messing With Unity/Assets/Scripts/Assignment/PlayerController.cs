d57c310439294a5c2ce11c4c1741e5b3ec07f84a
Colt Hedin <shatsky95@yahoo.com>
2019-04-17 17:01:08 -0600

    fixed the sliding issue

--- Messing With Unity/Assets/Scripts/Assignment/PlayerController.cs
+++ Messing With Unity/Assets/Scripts/Assignment/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour{
 // player movement variables
    public float moveSpeed;
    public float jumpHeight;
+   private bool doubleJump;
    //player grounded variables 
    private bool grounded; 
    public Transform groundCheck;
@@ -14,6 +15,9 @@ public class PlayerController : MonoBehaviour{
    public LayerMask whatIsGround;
 
    public Vector3 scale;
+   //non slide player
+   private float moveVelocity;
+   
 
    public Animator animator;
 
@@ -32,10 +36,12 @@ public class PlayerController : MonoBehaviour{
     // Update is called once per frame
     void Update(){
         //moves player left and right
+         moveVelocity = 0f;
 
     
         if(Input.GetKey(KeyCode.D)){
-            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            //GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            moveVelocity = moveSpeed;
             animator.SetBool("isWalking",true);
         }
 
@@ -43,16 +49,30 @@ public class PlayerController : MonoBehaviour{
             animator.SetBool("isWalking",false);
         }
         else if(Input.GetKey(KeyCode.A)){
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+           // GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+           moveVelocity = -moveSpeed;
             animator.SetBool("isWalking",true);
         }
         else if(Input.GetKeyUp (KeyCode.A)){
             animator.SetBool("isWalking",false);
         }
             //makes player jump
-        if(Input.GetKeyDown(KeyCode.W) && grounded ){
+        if(Input.GetKeyDown(KeyCode.W) && grounded )
+        {
+            Jump();}
+        if(grounded){
+            doubleJump = false;
+            animator.SetBool("isJumping",false);
+        }
+        if(Input.GetKeyDown (KeyCode.W)&& !doubleJump && !grounded){
             Jump();
-    }// make player flip
+            doubleJump = true;
+        }
+            //non slider player
+           
+            GetComponent<Rigidbody2D>().velocity =  new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+
+    // make player flip
         if(GetComponent<Rigidbody2D>().velocity.x > 0)
             transform.localScale = new Vector3(scale.x,scale.y,scale.z);
 
