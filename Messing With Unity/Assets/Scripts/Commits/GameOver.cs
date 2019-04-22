3c718dd48e96b0e71161da0eff930112b10903de
Colt Hedin <shatsky95@yahoo.com>
2019-04-22 15:21:45 -0600

    created game over screen

---
+++ Messing With Unity/Assets/Scripts/Commits/GameOver.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameOver : MonoBehaviour
+{
+     void OnCollisionEnter2D(Collision2D  other){
+         if(other.collider.tag==("Player"))
+         SceneManager.LoadScene("GameOver");
+         print("touch last tree");
+
+     } 
+}
